8eb8a23236c33ce386278ac381eba899cb940591
HachiDD12 <chenzhuofan123@gmail.com>
2022-11-14 21:07:35 -0500

    Update Enemy.cs

--- GameOffProject/Assets/Scripts/Enemy/Enemy.cs
+++ GameOffProject/Assets/Scripts/Enemy/Enemy.cs
@@ -3,19 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static Skill;
 
-public class Enemy: MonoBehaviour
+public class Enemy: EnemyStatus
 {
-    protected float MAX_HEALTH;
-    protected float health;
-    protected float happyATK;
-    protected float happyDEF;
-    protected float sadATK;
-    protected float sadDEF;
-    protected float angryATK;
-    protected float angryDEF;
-    protected int hitsTakenCounter;
-    protected int attackCounter;
-    protected List<Buff> buffs;
-    protected List<Debuff> debuffs;
-    protected List<Skill> skills;
+    
 }
