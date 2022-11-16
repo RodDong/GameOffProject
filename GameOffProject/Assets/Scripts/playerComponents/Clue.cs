4aff00cf4a646f2c5d1aa3698255e1017a18a26b
PlayX-2333 <59723577+PlayX-2333@users.noreply.github.com>
2022-11-15 19:32:18 -0500

    clue update

--- GameOffProject/Assets/Scripts/playerComponents/Clue.cs
+++ GameOffProject/Assets/Scripts/playerComponents/Clue.cs
@@ -8,7 +8,7 @@ public class Clue
     private string clueName;
     private int clueId;
     private string clueContent;
-    private string clueImageSource;
+    private string clueImageSource = "Art/UI/Clues/";
 
     public string Name { get { return clueName; } }
     public string Content { get { return clueContent; } }
@@ -23,14 +23,17 @@ public class Clue
             case -1:
                 clueName = "? ? ?";
                 clueContent = "? ? ?";
+
                 break;
             case 0:
                 clueName = "Clue 0";
                 clueContent = "Content of Clue 0";
+                clueImageSource += "Clue_0";
                 break;
             case 1:
                 clueName = "Clue 1";
                 clueContent = "Content of Clue 1";
+                clueImageSource += "Clue_1";
                 break;
             default:
                 clueName = "";
