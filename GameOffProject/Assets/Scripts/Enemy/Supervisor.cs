2e1f5db1b30502cdbe937ed74836969adce6a87d
JAAACKL <lzixuan@umich.edu>
2022-11-18 22:22:22 -0500

    Sample enemy logic
    Note: Supervisor is the name of the boss/enemy

---
+++ GameOffProject/Assets/Scripts/Enemy/Supervisor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Buff;
+
+public class EnemySupervisor : EnemyStatus
+{
+    int mana = 0;
+    int mana2 = 0;
+    // states
+    public override void MakeMove(PlayerStatus playerStatus)
+    {
+        // depending on state
+        if (mana == 5) {
+            Ultimate(playerStatus);
+        } else if (mana2 == 6) {
+            Secondary(playerStatus);
+        } else {
+            if (Random.Range(0f, 2f) < 1) {
+                AngryATK(playerStatus);
+            } else {
+                SadATK(playerStatus);
+            }
+        }
+
+        mana++;
+        mana2++;
+    }
+
+    private void Ultimate(PlayerStatus playerStatus) {
+
+    }
+
+    private void Secondary(PlayerStatus playerStatus) {
+
+    }
+
+    private void AngryATK(PlayerStatus playerStatus) {
+        float damageAmount = 10;
+        DealDamage(playerStatus, damageAmount, SkillAttribute.ANGRY);
+        playerStatus.ActivateBuff(new Buff(BuffId.WEAK));
+    }
+
+    private void SadATK(PlayerStatus playerStatus) {
+        float damageAmount = 10;
+        DealDamage(playerStatus, damageAmount, SkillAttribute.SAD);
+        playerStatus.ActivateBuff(new Buff(BuffId.WEAK));
+    }
+
+    public override void DealDamage(PlayerStatus playerStatus, float damage, SkillAttribute attribute)
+    {
+        base.DealDamage(playerStatus, damage, attribute);
+        mana++;
+    }
+}
