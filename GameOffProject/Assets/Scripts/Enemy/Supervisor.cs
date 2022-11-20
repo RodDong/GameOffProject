39a469a26e5262a3736aaf0bb51712472d4c4015
JAAACKL <lzixuan@umich.edu>
2022-11-20 01:32:40 -0500

    Update Supervisor.cs
    The supervisor enemy logic

--- GameOffProject/Assets/Scripts/Enemy/Supervisor.cs
+++ GameOffProject/Assets/Scripts/Enemy/Supervisor.cs
@@ -5,27 +5,29 @@ using static Buff;
 
 public class Supervisor : EnemyStatus
 {
-    int mana = 0;
-    int mana2 = 0;
+    int ultimateCD = 0;
+    int fortifyCD = 5;
     // states
     public override void MakeMove(PlayerStatus playerStatus)
     {
         // depending on state
-        if (mana == 50) {
-            mana = 0;
+        if (ultimateCD == 10) {
+            ultimateCD = 0;
             Ultimate(playerStatus);
-        } else if (mana2 == 6) {
+        } else if (fortifyCD == 5) {
+            fortifyCD = 0;
             Secondary(playerStatus);
         } else {
-            if (Random.Range(0f, 2f) < 1) {
+            float playerAngryDEF = playerStatus.getDEFbyAttribute(SkillAttribute.ANGRY);
+            float playerSadDEF = playerStatus.getDEFbyAttribute(SkillAttribute.SAD);
+            if (playerAngryDEF < playerSadDEF) {
                 AngryATK(playerStatus);
             } else {
                 SadATK(playerStatus);
             }
         }
-
-        mana++;
-        mana2++;
+        ultimateCD++;
+        fortifyCD++;
     }
 
     private void Ultimate(PlayerStatus playerStatus) {
@@ -33,23 +35,23 @@ public class Supervisor : EnemyStatus
     }
 
     private void Secondary(PlayerStatus playerStatus) {
-
+        ActivateBuff(new Buff(BuffId.FORTIFIED));
     }
 
     private void AngryATK(PlayerStatus playerStatus) {
         float damageAmount = 10;
         DealDamage(playerStatus, damageAmount, SkillAttribute.ANGRY);
+        Buff buff = new Buff(BuffId.WEAK);
+        buff.SetDefenseReduction(10, SkillAttribute.SAD);
         playerStatus.ActivateBuff(new Buff(BuffId.WEAK));
     }
 
     private void SadATK(PlayerStatus playerStatus) {
         float damageAmount = 10;
         DealDamage(playerStatus, damageAmount, SkillAttribute.SAD);
+        Buff buff = new Buff(BuffId.WEAK);
+        buff.SetDefenseReduction(10, SkillAttribute.ANGRY);
         playerStatus.ActivateBuff(new Buff(BuffId.WEAK));
     }
 
-    public override void DealDamage(PlayerStatus playerStatus, float damage, SkillAttribute attribute)
-    {
-        base.DealDamage(playerStatus, damage, attribute);
-    }
 }
