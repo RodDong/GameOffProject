b5b0909573c57f4028d2efe58e9da3c7ff972f4e
PassbyKamenRider <PassbyKamenRider@outlook.com>
2022-11-22 01:12:42 -0600

    Finish doctor logic

---
+++ GameOffProject/Assets/Scripts/Enemy/Doctor.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Effect;
+
+public class Doctor : EnemyStatus
+{
+    private Queue<int> castOrder = new Queue<int>();
+    private int UltimateCd = 10;
+    private void Awake() {
+        MAX_HEALTH = 150.0f;
+        currentHealth = MAX_HEALTH;
+    }
+    public override void MakeMove(PlayerStatus playerStatus) {
+        if (UltimateCd == 0) {
+            Ultimate(playerStatus);
+            UltimateCd = 10;
+            return;
+        }
+
+        if (castOrder.Count != 0)
+        {
+            int skillId = castOrder.Dequeue();
+            switch(skillId) {
+                case 0:
+                    Secondary(playerStatus);
+                    break;
+                case 1:
+                    DefReduceAttack(playerStatus);
+                    break;
+                case 2:
+                    AtkReduceAttack(playerStatus);
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            int roundsBeforeSed = Random.Range(3,4);
+            bool hasReducedDef = false;
+            for (int i = 0; i < roundsBeforeSed; i++) {
+                if (hasReducedDef) {
+                    castOrder.Enqueue(2);
+                } else {
+                    int randomBasicMove = Random.Range(1,2);
+                    if (randomBasicMove == 1) {
+                        hasReducedDef = true;
+                    }
+                    castOrder.Enqueue(randomBasicMove);
+                }
+            }
+            castOrder.Enqueue(0);
+        }
+
+        UltimateCd -= 1;
+    }
+
+
+#region doctor skills
+
+    private void Ultimate(PlayerStatus playerStatus) {
+        Debug.Log("Enemy cast ultimate");
+        playerStatus.ActivateEffect(new Effect(EffectId.DISMEMBERED));
+        ProcessHealing(playerStatus.GetMaxHealth() / 3.0f);
+    }
+
+    private void Secondary(PlayerStatus playerStatus) {
+        Debug.Log("Enemy cast steal");
+        Effect stolenEffect = new Effect(EffectId.STOLEN);
+        stolenEffect.SetStolenAmount(playerStatus.getATKbyAttribute(SkillAttribute.ANGRY) * 0.5f, SkillAttribute.ANGRY);
+        stolenEffect.SetStolenAmount(playerStatus.getATKbyAttribute(SkillAttribute.SAD) * 0.5f, SkillAttribute.SAD);
+        stolenEffect.SetStolenAmount(playerStatus.getATKbyAttribute(SkillAttribute.HAPPY) * 0.5f, SkillAttribute.HAPPY);
+        playerStatus.ActivateEffect(stolenEffect);
+    }
+
+    private void DefReduceAttack(PlayerStatus playerStatus) {
+        Debug.Log("Enemy cast def reduce");
+        Effect weakEffect = new Effect(EffectId.WEAK);
+        weakEffect.SetDefenseReduction(20.0f, SkillAttribute.SAD);
+        weakEffect.SetDefenseReduction(0.0f, SkillAttribute.HAPPY);
+        weakEffect.SetDefenseReduction(0.0f, SkillAttribute.ANGRY);
+        playerStatus.ActivateEffect(weakEffect);
+    }
+
+    private void AtkReduceAttack(PlayerStatus playerStatus) {
+        Debug.Log("Enemy cast atk reduce");
+        Effect reducedEffect = new Effect(EffectId.REDUCED);
+        reducedEffect.SetAttackReduction(20.0f, SkillAttribute.SAD);
+        reducedEffect.SetAttackReduction(0.0f, SkillAttribute.HAPPY);
+        reducedEffect.SetAttackReduction(0.0f, SkillAttribute.ANGRY);
+        playerStatus.ActivateEffect(reducedEffect);
+        playerStatus.TakeDamage(20.0f, SkillAttribute.SAD);
+    }
+
+#endregion
+}
