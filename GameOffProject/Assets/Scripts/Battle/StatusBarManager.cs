c6988aab0917c1496f81542c4ca936d450304e81
PassbyKamenRider <PassbyKamenRider@outlook.com>
2022-11-22 00:12:51 -0600

    Change some namings

--- GameOffProject/Assets/Scripts/Battle/StatusBarManager.cs
+++ GameOffProject/Assets/Scripts/Battle/StatusBarManager.cs
@@ -22,19 +22,19 @@ public class StatusBarManager : MonoBehaviour
         
     }
 
-    public void ShowBuffDeBuffDetails(int i)
+    public void ShowBuffDeBuffDetails(int index)
     {
-        GameObject effectIcon = battleManager.GetEffectTransfroms()[i - 1].gameObject;
+        GameObject effectIcon = battleManager.GetEffectTransfroms()[index].gameObject;
         GameObject descriptionMenu = effectIcon.GetComponentsInChildren<RectTransform>(true)[1].gameObject;
         descriptionMenu.SetActive(true);
-        string description = playerStatus.GetActiveEffects()[i-1].GetDescription();
+        string description = playerStatus.GetActiveEffects()[index].GetDescription();
         descriptionMenu.GetComponent<TextMeshProUGUI>().text = description;
-        descriptionMenu.GetComponent<Transform>().position = new Vector2(Input.mousePosition.x + 10.0f, Input.mousePosition.y + 10.0f);
+        descriptionMenu.GetComponent<RectTransform>().position = new Vector2(Input.mousePosition.x + 10.0f, Input.mousePosition.y + 10.0f);
     }
 
-    public void HideBuffDeBuffDetails(int i)
+    public void HideBuffDeBuffDetails(int index)
     {
-        GameObject effectIcon = battleManager.GetEffectTransfroms()[i - 1].gameObject;
+        GameObject effectIcon = battleManager.GetEffectTransfroms()[index].gameObject;
         GameObject descriptionMenu = effectIcon.GetComponentsInChildren<RectTransform>(true)[1].gameObject;
         descriptionMenu.SetActive(false);
     }
