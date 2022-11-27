b28341668c54f8488b5ce9fe3a0ae79869814884
Xiaolong Dong <dongxiao@usc.edu>
2022-11-26 18:20:38 -0800

    modified scenes

--- GameOffProject/Assets/Scripts/SceneManager/BlackScreenManager.cs
+++ GameOffProject/Assets/Scripts/SceneManager/BlackScreenManager.cs
@@ -8,7 +8,7 @@ public class BlackScreenManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(true);
     }
 
     // Update is called once per frame
