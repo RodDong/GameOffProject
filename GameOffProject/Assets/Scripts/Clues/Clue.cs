781ffdeccd2464ec2dae69c73b00183d1468dce6
JAAACKL <lzixuan@umich.edu>
2022-11-28 17:07:12 -0500

    Update Clue.cs

--- GameOffProject/Assets/Scripts/Clues/Clue.cs
+++ GameOffProject/Assets/Scripts/Clues/Clue.cs
@@ -56,48 +56,43 @@ public class Clue
                 clueImageSource += "Clue_3";
                 break;
             case 4:
-                clueName = "Sim's Pictures";
-                clueDescription = "heart? Lung? brain? What are these for?";
-                clueImageSource += "Clue_4";
-                break;
-            case 5:
                 clueName = "Sim's Feast Menu";
                 clueDescription = "The one that he told me before. But the menu looks not like a usual restaurant.";
                 clueImageSource += "menu";
                 break;
-            case 6:
-                clueName = "Sim's Records";
+            case 5:
+                clueName = "Sim's Phone";
                 clueDescription = "A list of surgery records. This signature looks familiar.";
-                clueImageSource += "Clue_6";
+                clueImageSource += "Cat_Phone";
                 break;
-            case 7:
+            case 6:
                 clueName = "Boss' Notebook";
                 clueDescription = "The steak has a very special taste. \n" + 
                 "I went to ask Mr. P about it and he told me it was carefully selected from human legs. It is so delicious. It is the taste of power and higher class. I will become one of them in the future.";
                 clueImageSource += "Notebook";
                 break;
-            case 8:
+            case 7:
                 clueName = "Boss' Phone";
                 clueDescription = "Phone is locked. There is an unread message: \n From Sim: Wanna have a joyful night together? See you at the same place~";
                 clueImageSource += "Boss_phone";
                 break;
-            case 9:
+            case 8:
                 clueName = "Boss' Ledger";
                 clueDescription = "It is a ledger that records the embezzlement of public funds by Mr. L to subsidize the doctor.";
                 clueImageSource += "Account_book";
                 break;
-            case 10:
+            case 9:
                 clueName = "Doctor's Phone";
                 clueDescription = "The Doctor's phone. Someone is sending him a message. \n " + 
                 "Mr. L: \"See you outside Thyrsus, as always. Remember to bring my pay-checks. Also, don't forget to let me know what to get next month.\"";
                 clueImageSource += "Doctor_phone";
                 break;
-            case 11:
+            case 10:
                 clueName = "Doctor's Name List";
                 clueDescription = "Proud sponsors and Investors of Maston Clinics. Has the boss's name on it with a decent amount of money.";
                 clueImageSource += "Doctor_notebook";
                 break;
-            case 12:
+            case 11:
                 clueName = "Doctor's Cargo";
                 clueDescription = "Deliver to FEAST. \n In need of lungs... hearts...";
                 clueImageSource += "Doctor_file";
