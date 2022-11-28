819f797e4ba42f0e2364428d93f791192c937a5f
JAAACKL <lzixuan@umich.edu>
2022-11-27 23:21:06 -0500

    Clue Assets

--- GameOffProject/Assets/Scripts/Clues/Clue.cs
+++ GameOffProject/Assets/Scripts/Clues/Clue.cs
@@ -38,17 +38,17 @@ public class Clue
             case 0:
                 clueName = "Chef's Phone";
                 clueDescription = "A black, clean design. Probably going to cost many times your monthly paycheck.";
-                clueImageSource += "Clue_0";
+                clueImageSource += "chef_phone";
                 break;
             case 1:
                 clueName = "Chef's Guest List";
                 clueDescription = "A list of names. An invitation letter, maybe? Those last names…some seem familiar, you’ve seen them in newspapers before, those big, power families. The boss's name is in it.";
-                clueImageSource += "Clue_1";
+                clueImageSource += "Guest_list";
                 break;
             case 2:
                 clueName = "Chef's Supply List";
                 clueDescription = "A supply list, sent from the doctor.\n...Lung, from a 27 years old male...\n...left hand, from a 67 years old male...\n...Intestine, from a 42 years old female....";
-                clueImageSource += "Clue_2";
+                clueImageSource += "Chef_file";
                 break;
             case 3:
                 clueName = "Sim's List";
@@ -63,7 +63,7 @@ public class Clue
             case 5:
                 clueName = "Sim's Feast Menu";
                 clueDescription = "The one that he told me before. But the menu looks not like a usual restaurant.";
-                clueImageSource += "Clue_5";
+                clueImageSource += "menu";
                 break;
             case 6:
                 clueName = "Sim's Records";
@@ -72,24 +72,25 @@ public class Clue
                 break;
             case 7:
                 clueName = "Boss' Notebook";
-                clueDescription = "Content of Clue 7";
-                clueImageSource += "Clue_7";
+                clueDescription = "The steak has a very special taste. \n" + 
+                "I went to ask Mr. P about it and he told me it was carefully selected from human legs. It is so delicious. It is the taste of power and higher class. I will become one of them in the future.";
+                clueImageSource += "Notebook";
                 break;
             case 8:
                 clueName = "Boss' Phone";
                 clueDescription = "Phone is locked. There is an unread message: ";
-                clueImageSource += "Clue_8";
+                clueImageSource += "Boss_phone";
                 break;
             case 9:
                 clueName = "Boss' Ledger";
                 clueDescription = "It is a ledger that records the embezzlement of public funds by Mr. L to subsidize the doctor.";
-                clueImageSource += "Clue_9";
+                clueImageSource += "Account_book";
                 break;
             case 10:
                 clueName = "Doctor's Phone";
                 clueDescription = "The Doctor's phone. Someone is sending him a message. \n " + 
                 "Mr. L: \"See you outside Thyrsus, as always. Remember to bring my pay-checks. Also, don't forget to let me know what to get next month.\"";
-                clueImageSource += "Clue_10";
+                clueImageSource += "Cat_Phone";
                 break;
             case 11:
                 clueName = "Doctor's Name List";
