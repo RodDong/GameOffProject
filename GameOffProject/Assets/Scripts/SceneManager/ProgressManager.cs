58f8691ed9cbe559fe1b2e250b355892328378c9
JAAACKL <lzixuan@umich.edu>
2022-11-29 00:22:12 -0500

    Progress Manager

---
+++ GameOffProject/Assets/Scripts/SceneManager/ProgressManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressManager : MonoBehaviour
+{
+    public int currentProgress = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // currentProgress to be read from json
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
